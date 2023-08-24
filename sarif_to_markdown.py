9bb1510fee10832033b0ccf20beb257df19459b5
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-24 15:35:29 +0530

    Update sarif_to_markdown.py

--- sarif_to_markdown.py
+++ sarif_to_markdown.py
@@ -1,8 +1,8 @@
 import json
 import sys
 
-def sarif_to_markdown(cx.sarif):
-    with open(cx.sarif, 'r') as f:
+def sarif_to_markdown(sarif_file):
+    with open(sarif_file, 'r') as f:
         sarif_data = json.load(f)
 
     markdown_output = ""
@@ -10,9 +10,9 @@ def sarif_to_markdown(cx.sarif):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python convert_sarif_to_md.py <sarif_file>")
+        print("Usage: python sarif_to_markdownd.py <sarif_file>")
         sys.exit(1)
     
     sarif_file = sys.argv[1]
-    markdown_content = sarif_to_markdown(cx.sarif)
+    markdown_content = sarif_to_markdown(sarif_file)
     print(markdown_content)
