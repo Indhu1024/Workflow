6d86677f6043d5bdfb9944f4e302f20b4a15eab1
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-28 15:00:50 +0530

    Update convert_sarif_to_md.py

--- convert_sarif_to_md.py
+++ convert_sarif_to_md.py
@@ -3,7 +3,7 @@ import json
 def sarif_to_markdown(sarif_file):
     with open(sarif_file, 'r') as f:
         sarif_data = json.load(f)
-
+    print(json.dumps(sarif_data, indent=2))
     markdown_output = "# SARIF Report\n\n"
     
     for run in sarif_data.get('runs', []):
@@ -15,7 +15,9 @@ def sarif_to_markdown(sarif_file):
     return markdown_output
 
 if __name__ == '__main__':
-    sarif_file = 'cx.sarif'
+    if len(sys.argv) != 2:
+        print("Usage: python convert_sarif_to_md.py <sarif_file>")
+        sys.exit(1)
+    sarif_file = sys.argv[1]
     markdown_content = sarif_to_markdown(sarif_file)
-    with open('converted_report.md', 'w') as f:
-        f.write(markdown_content)
+    print(markdown_content)
