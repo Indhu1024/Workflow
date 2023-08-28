9bea27ddfb4c32adaf6beee5413782ef53ed1ef7
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-28 14:51:00 +0530

    Update convert_sarif_to_md.py

--- convert_sarif_to_md.py
+++ convert_sarif_to_md.py
@@ -1,22 +1,21 @@
 import json
-import sys
 
 def sarif_to_markdown(sarif_file):
     with open(sarif_file, 'r') as f:
         sarif_data = json.load(f)
 
-    markdown_output = ""
+    markdown_output = "# SARIF Report\n\n"
     
-    # Process SARIF data and generate Markdown content
-    # Customize this part according to your needs
-    
+    for run in sarif_data.get('runs', []):
+        for result in run.get('results', []):
+            markdown_output += f"## Result: {result.get('ruleId', 'Unknown')}\n"
+            markdown_output += f"**Message:** {result.get('message', '')}\n"
+            markdown_output += f"**Level:** {result.get('level', {}).get('name', '')}\n\n"
+
     return markdown_output
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python convert_sarif_to_md.py <sarif_file>")
-        sys.exit(1)
-    
-    sarif_file = sys.argv[1]
+    sarif_file = 'cx.sarif'
     markdown_content = sarif_to_markdown(sarif_file)
-    print(markdown_content)
+    with open('converted_report.md', 'w') as f:
+        f.write(markdown_content)
