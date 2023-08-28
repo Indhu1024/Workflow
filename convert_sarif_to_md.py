92923d12e830cc5db3a1225e347556c7f8bf6b2b
Indhu1024 <83407978+Indhu1024@users.noreply.github.com>
2023-08-28 15:54:33 +0530

    Update convert_sarif_to_md.py

--- convert_sarif_to_md.py
+++ convert_sarif_to_md.py
@@ -9,6 +9,7 @@ def sarif_to_markdown(sarif_file):
     
     for run in sarif_data.get('runs', []):
         for result in run.get('results', []):
+           print(result)
            level = result.get('level', {}).get('name', 'N/A')
            message = result.get('message', 'No message')
             
