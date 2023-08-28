0ea268854e9490b38d5307b0e44ef9d9a4514f85
ibalacha <Indhumathi.Balachandran@genesys.com>
2023-08-28 16:15:54 +0530

    Update convert_sarif_to_md.py

--- convert_sarif_to_md.py
+++ convert_sarif_to_md.py
@@ -1,24 +1,27 @@
 import json
 import sys
+
+    
 def sarif_to_markdown(sarif_file):
     with open(sarif_file, 'r') as f:
         sarif_data = json.load(f)
-    print("SARIF Data:")    
-    print(json.dumps(sarif_data, indent=2))
-    markdown_output = "# SARIF Report\n\n"
+
+    markdown_output = ""
     
     for run in sarif_data.get('runs', []):
         for result in run.get('results', []):
-            print(result)
+            print(result)  # Print the result to debug
             message = result.get('message', {})
             rule_id = result.get('ruleId', 'N/A')
             level = result.get('level', {}).get('name', 'N/A')
             location = result.get('locations', [])[0].get('physicalLocation', {}).get('artifactLocation', {}).get('uri', 'N/A')
             
+            # Customize the formatting and content according to your needs
             markdown_output += f"**Rule ID:** {rule_id}\n"
             markdown_output += f"**Level:** {level}\n"
             markdown_output += f"**Location:** {location}\n"
             markdown_output += f"**Message:** {message.get('text', 'No message')}\n\n"
+    
     return markdown_output
 
 if __name__ == '__main__':
