f91747bfe8b600c77790e78d043e1ce4d3bac26c
ibalacha <Indhumathi.Balachandran@genesys.com>
2023-08-31 15:34:56 +0530

    Update generate_table_pcc.py

--- generate_table_pcc.py
+++ generate_table_pcc.py
@@ -1,52 +1,13 @@
 import json
-
+import os
+with open('pcc_scan_results.json', 'r') as json_file:
+# Parse JSON data
+   data = json.loads(json_data)
 # JSON data (replace this with your data)
-json_data = '''
-{
-    "results": [
-        {
-            "id": "sha256:bd08b0c17e93272cda54d566f0597f5d4196c52f95ad5e3b49d2be31c7d11dd2",
-            "name": "gws-oci-dev.artifactory.gws.genesys.com/gws-api-v2:8.6.000.00-0007",
-            "distro": "Red Hat Enterprise Linux release 8.8 (Ootpa)",
-            "distroRelease": "RHEL8",
-            "digest": "sha256:35a9110d259c99a16430aa28f86f5435a5e01fa0db44a401434a17e038b2fc7f",
-            "collections": [
-                "All"
-            ],
-            "packages": [
-                {
-                    "type": "os",
-                    "name": "langpacks-ja",
-                    "version": "1.0-12.el8",
-                    "licenses": [
-                        "GPLv2+"
-                    ]
-                },
-                {
-                    "type": "os",
-                    "name": "tzdata",
-                    "version": "2023c-1.el8",
-                    "licenses": [
-                        "Public Domain"
-                    ]
-                },
-                {
-                    "type": "os",
-                    "name": "nss-tools",
-                    "version": "3.79.0-11.el8_7",
-                    "licenses": [
-                        "MPLv2.0"
-                    ]
-                }
-                # Add more package entries here
-            ]
-        }
-    ]
-}
-'''
+
 
 # Parse JSON data
-data = json.loads(json_data)
+#data = json.loads(json_data)
 
 # Extract the packages list
 packages = data["results"][0]["packages"]
