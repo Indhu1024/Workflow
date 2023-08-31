14d80c95be0aa06aa74389cdfa70bab9d81edf42
ibalacha <Indhumathi.Balachandran@genesys.com>
2023-08-31 16:14:07 +0530

    Update generate_table_pcc.py

--- generate_table_pcc.py
+++ generate_table_pcc.py
@@ -8,15 +8,19 @@ with open('pcc_scan_results.json', 'r') as json_file:
 # Generate HTML table
 table_html = '<table>'
 table_html += '<tr><th>Type</th><th>Name</th><th>Version</th><th>Licenses</th></tr>'
-for type,name,version,licenses in data.items():
+for package in data['results'][0]['packages']:
+    package_type = package['type']
+    package_name = package['name']
+    package_version = package['version']
+    package_licenses = ', '.join(package['licenses'])
     table_html += f'''
     <tr>
-        <td>{type}</td>
-        <td>{name}</td>
-        <td>{version}</td>
-        <td>{licenses}</td>
+        <td>{package_type}</td>
+        <td>{package_name}</td>
+        <td>{package_version}</td>
+        <td>{package_licenses}</td>
     </tr>
-'''
+    '''
 table_html += '</table>'
 output_path = 'pcc_table.html'
 # Save HTML to a file
