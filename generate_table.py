9d6ad336c07355259c3a5a9181f302c8b9c8d0b8
ibalacha <Indhumathi.Balachandran@genesys.com>
2023-08-31 14:23:22 +0530

    Update generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -16,6 +16,8 @@ for key, value in data.items():
     '''
 table_html += '</table>'
 print(table_html)
+print("Current Working Directory:", os.getcwd())
+
 output_path=r'C:\Users\ibalacha\Downloads\Workflow repository\Workflow\Workflow'
 # Write the generated HTML to a file
 with open(output_path, 'w') as html_file:
