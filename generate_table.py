73bf5ee29f4a41a64c7591453232a57012c5d526
ibalacha <Indhumathi.Balachandran@genesys.com>
2023-08-31 15:55:00 +0530

    Update generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -1,5 +1,6 @@
 import json
 import os
+
 # Read JSON data from the file
 with open('example_1.json', 'r') as json_file:
     data = json.load(json_file)
@@ -15,14 +16,12 @@ for key, value in data.items():
     </tr>
     '''
 table_html += '</table>'
-#print(table_html)
-#print("Current Working Directory:", os.getcwd())
 
-output_path=r'C:\Users\ibalacha\Downloads\Workflow repository\Workflow\Workflow\table.html'
-#output_dir = os.path.dirname(output_path)
-#os.makedirs(output_dir, exist_ok=True)
-# Write the generated HTML to a file
-with open(output_path, 'w',encoding='utf-8') as html_file:
+# Specify the complete output file path, including the file name
+output_path = 'table.html'
+
+# Write the generated HTML to the specified output path
+with open(output_path, 'w') as html_file:
     html_file.write(table_html)
 
 print("HTML table generated and saved to", output_path)
